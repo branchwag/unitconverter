e101ca774b2f91bd13ed2ac9420d99e7934845e4
LeAnne B. <branchleannelmb@gmail.com>
2024-11-29 06:48:02 -0500

    sets up frame

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,32 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		JLabel label = new JLabel();
 		
+		label.setText("What would you like to convert?");
+		
+		frame.setTitle("Unit Converter");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500,500);
 		frame.setVisible(true);
-
+		frame.getContentPane().setBackground(Color.black);
+		frame.add(label);
+		
+		//m to ft 3.28
+		//ft to inches 12
+		//meters to inches
+		
+		//hr to min
+		//min to seconds
+		
+		//error if distance to time
+		
 	}
 
 }
